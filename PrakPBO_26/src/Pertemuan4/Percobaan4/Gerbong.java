741f39ebd65c7a27ba67186307bf1bed14e0013e
galangMaheswara <ullyses88@gmail.com>
2026-09-23 18:41:40 +0700

    Percobaan4Pertanyaan

--- PrakPBO_26/src/Pertemuan4/Percobaan4/Gerbong.java
+++ PrakPBO_26/src/Pertemuan4/Percobaan4/Gerbong.java
@@ -17,7 +17,11 @@ public class Gerbong {
     }
 
     public void setPenumpang(Penumpang penumpang, int nomor){
-        this.arrayKursi[nomor-1].setPenumpang(penumpang);
+        if (this.arrayKursi[nomor-1].getPenumpang() != null) {
+            System.out.println("Occupied!");
+        } else{
+            this.arrayKursi[nomor-1].setPenumpang(penumpang);
+        }
     }
 
     public String info(){
--- PrakPBO_26/src/Pertemuan4/Percobaan4/MainPercobaan4.java
+++ PrakPBO_26/src/Pertemuan4/Percobaan4/MainPercobaan4.java
@@ -2,9 +2,11 @@ package Pertemuan4.Percobaan4;
 
 public class MainPercobaan4 {
     public static void main(String[] args) {
-        Penumpang p = new Penumpang("12345", "Mr. Krab");
+        Penumpang p1 = new Penumpang("12345", "Mr. Krab");
+        Penumpang p2 = new Penumpang("124", "Budi");
         Gerbong gerbong = new Gerbong("A", 10);
-        gerbong.setPenumpang(p, 1);
+        gerbong.setPenumpang(p1, 1);
+        gerbong.setPenumpang(p2, 1);
         System.out.println(gerbong.info());
     }
 }

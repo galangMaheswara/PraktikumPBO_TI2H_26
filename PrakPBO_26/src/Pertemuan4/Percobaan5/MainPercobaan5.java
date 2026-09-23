3941995588db9a1d3ccaa7107e5dff358c5b7eea
galangMaheswara <ullyses88@gmail.com>
2026-09-23 20:15:48 +0700

    Percobaan5

---
+++ PrakPBO_26/src/Pertemuan4/Percobaan5/MainPercobaan5.java
@@ -0,0 +1,8 @@
+package Pertemuan4.Percobaan5;
+
+public class MainPercobaan5 {
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil("Avanza");
+        mobil.tampilkanInfo();
+    }
+}
---
+++ PrakPBO_26/src/Pertemuan4/Percobaan5/Mesin.java
@@ -0,0 +1,13 @@
+package Pertemuan4.Percobaan5;
+
+public class Mesin {
+    private String tipe;
+
+    public Mesin() {
+        this.tipe = "4-silinder";
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+}
